3cfb0e3400e64ee3685befe315efe6b30f6a2f43
David Lanza <dlanza1005@gmail.com>
2023-11-20 20:51:47 -0500

    First commit!

---
+++ NPC_sprites.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="NPC_sprites" tilewidth="16" tileheight="22" tilecount="120" columns="12">
+ <image source="NPC_sprites.png" trans="73c5a5" width="192" height="220"/>
+ <tile id="72" type="NPC"/>
+ <tile id="119">
+  <animation>
+   <frame tileid="108" duration="200"/>
+   <frame tileid="111" duration="200"/>
+   <frame tileid="108" duration="200"/>
+   <frame tileid="112" duration="200"/>
+   <frame tileid="109" duration="200"/>
+   <frame tileid="113" duration="200"/>
+   <frame tileid="109" duration="200"/>
+   <frame tileid="114" duration="200"/>
+   <frame tileid="110" duration="200"/>
+   <frame tileid="115" duration="200"/>
+   <frame tileid="110" duration="200"/>
+   <frame tileid="116" duration="200"/>
+   <frame tileid="118" duration="200"/>
+   <frame tileid="117" duration="200"/>
+   <frame tileid="118" duration="200"/>
+   <frame tileid="119" duration="200"/>
+   <frame tileid="108" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
